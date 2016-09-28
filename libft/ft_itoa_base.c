d9fa4fd605eb91a1cb26d4d367235327baff54f9
Simon HILL <shill@e2r2p1.42.fr>
2016-09-28 15:12:51 +0200

    itoa_base is not working properly. Need to fix it fast

--- libft/ft_itoa_base.c
+++ libft/ft_itoa_base.c
@@ -12,7 +12,9 @@
 
 #include "libft.h"
 
-static  int get_char(int c)
+#define	RADIX		"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+/*static  int get_char(int c)
 {
 	if (c >= 10 && c <= 15)
 		return (c + 87);
@@ -20,7 +22,7 @@ static  int get_char(int c)
 		return (c + 48);
 }
 
-static	char	*do_conversion(unsigned int x, int base, int n, char *str)
+static	char	*do_conversion(uintmax_t x, int base, int n, char *str)
 {
 	int	d;
 	int	i;
@@ -37,7 +39,7 @@ static	char	*do_conversion(unsigned int x, int base, int n, char *str)
 	return (str);
 }
 
-char	*ft_itoa_base(unsigned int x, int base)
+char	*ft_itoa_base(uintmax_t x, int base)
 {
 	int		n;
 	char	*str;
@@ -51,4 +53,48 @@ char	*ft_itoa_base(unsigned int x, int base)
 	str = ft_strnew(n);
 	str = do_conversion(x, base, n, str);
 	return (str);
+}*/
+
+
+/* ------- OTHER VERSION ----- */
+/*static size_t	nbrlen(int input, unsigned int radix)
+{
+  size_t	i;
+
+  i = 1;
+  if (input < 0)
+    ++i;
+  while (radix > 1 && (input /= radix))
+    ++i;
+  return (i);
 }
+
+void		ft_itoa_base(int input, char *buffer, unsigned int radix)
+{
+  size_t	i;
+
+  i = nbrlen(input, radix) - 1;
+  buffer[i] = '\0';
+  if (input < 0)
+    buffer[0] = '-';
+  while ((input))
+    {
+      buffer[i] = RADIX[ft_abs(input) % radix];
+      input /= radix;
+      --i;
+    }
+}
+
+char *		itoadup(int input, unsigned int radix)
+{
+  char *	buffer;
+
+  if (radix <= 1
+      || radix >= (int)sizeof(RADIX)
+      || !(buffer = malloc(sizeof(*buffer)
+			   * ((nbrlen(input, radix) - 1) + 2))))
+    return (NULL);
+  ft_itoa_base(input, buffer, radix);
+  return (buffer);
+}*/
+
